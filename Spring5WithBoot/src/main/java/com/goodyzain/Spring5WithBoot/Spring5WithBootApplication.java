d07825dc21db19f288d25a236fb7f851b1c44856
goodyzain <goodyzain@gmail.com>
2018-11-14 21:54:11 +0530

    Spring 5 with Boot 2 first commit!

---
+++ Spring5WithBoot/src/main/java/com/goodyzain/Spring5WithBoot/Spring5WithBootApplication.java
@@ -0,0 +1,12 @@
+package com.goodyzain.Spring5WithBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WithBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5WithBootApplication.class, args);
+	}
+}
